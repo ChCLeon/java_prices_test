eecf3b04e20428833d2d6f13b379f84f5ee15c3f
ChristianCalvo <calvoleon.christian@gmail.com>
2025-01-21 13:52:53 +0100

    Add ProductPrices DTO

---
+++ src/main/java/com/example/demo/domain/DTO/ProductPricesDTO.java
@@ -0,0 +1,89 @@
+package com.example.demo.domain.DTO;
+
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+public class ProductPricesDTO {
+    private Long id;
+    private Long brand_id;
+    private LocalDateTime start_date;
+    private LocalDateTime end_date;
+    private Long price_list;
+    private int product_id;
+    private int priority;
+    private float price;
+    private String currency;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBrand_id() {
+        return brand_id;
+    }
+
+    public void setBrand_id(Long brand_id) {
+        this.brand_id = brand_id;
+    }
+
+    public LocalDateTime getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDateTime start_date) {
+        this.start_date = start_date;
+    }
+
+    public LocalDateTime getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDateTime end_date) {
+        this.end_date = end_date;
+    }
+
+    public Long getPrice_list() {
+        return price_list;
+    }
+
+    public void setPrice_list(Long price_list) {
+        this.price_list = price_list;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}

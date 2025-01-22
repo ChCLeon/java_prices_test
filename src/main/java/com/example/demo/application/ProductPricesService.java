cf2230a658496edf88fc0ab62d46ebc3aacd4bcd
ChristianCalvo <calvoleon.christian@gmail.com>
2025-01-22 09:27:06 +0100

    Add implementation of repository findByProductByPrice method using optional for null values and avoiding nullpointexception

--- src/main/java/com/example/demo/application/ProductPricesService.java
+++ src/main/java/com/example/demo/application/ProductPricesService.java
@@ -1,8 +1,12 @@
 package com.example.demo.application;
 
+import com.example.demo.domain.entity.ProductPrices;
 import com.example.demo.infrastructure.repository.ProductPricesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class ProductPricesService {
@@ -13,5 +17,9 @@ public class ProductPricesService {
         this.productPricesRepository = productPricesRepository;
     }
 
-    
+    public Optional<ProductPrices> getPrice(Long brand_id, Long product_id, LocalDateTime applicationDate) {
+        return productPricesRepository.findProductByPrice(brand_id, product_id, applicationDate)
+                .stream()
+                .findFirst();
+    }
 }

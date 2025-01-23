bb332fce46bbee34729af156e0339dcff9ae18bc
ChristianCalvo <calvoleon.christian@gmail.com>
2025-01-23 13:42:01 +0100

    Green getEmptyPrice test

--- src/test/java/com/example/demo/application/ProductPricesServiceTest.java
+++ src/test/java/com/example/demo/application/ProductPricesServiceTest.java
@@ -1,10 +1,16 @@
 package com.example.demo.application;
 
+import com.example.demo.domain.entity.ProductPrices;
 import com.example.demo.infrastructure.repository.ProductPricesRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -18,6 +24,11 @@ class ProductPricesServiceTest {
 
     @Test
     void getEmptyPrice(){
-
+        List<ProductPrices> productPricesList =new ArrayList<>();
+        Long brand_id = null;
+        Long product_id = null;
+        LocalDateTime applicationDate = null;
+        Mockito.when(productPricesRepository.findProductByPrice(brand_id,product_id,applicationDate)).thenReturn(productPricesList);
+        assertEquals(productPricesList,List.of());
     }
 }
